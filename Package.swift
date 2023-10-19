ab620244ba0dd300afd690e1cb710c175f358e08
Tola Voeung <tolacambo@gmail.com>
2023-10-19 14:20:26 +0700

    added SocialLogin package

--- Package.swift
+++ Package.swift
@@ -13,10 +13,17 @@ let package = Package(
         .library(
             name: "CosyncJWTSwift",
             targets: ["CosyncJWTSwift"]),
+        .library(
+            name: "CosyncGoogleAuth",
+            targets: ["CosyncGoogleAuth"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            url: "https://github.com/google/GoogleSignIn-iOS",
+            from: "7.0.0"
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,6 +31,10 @@ let package = Package(
         .target(
             name: "CosyncJWTSwift",
             dependencies: []),
+        .target(
+            name: "CosyncGoogleAuth",
+            dependencies: [.product(name: "GoogleSignInSwift", package: "googlesignin-ios")],
+            path: "Sources/CosyncSocialAuth"),
         .testTarget(
             name: "CosyncJWTSwiftTests",
             dependencies: ["CosyncJWTSwift"]),
---
+++ Sources/CosyncSocialAuth/CosyncGoogleAuth.swift
@@ -0,0 +1,104 @@
+//
+//  CSGoogleAuth.swift
+//  CosyncJWTiOS
+//
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
+//
+//  Created by Tola VOEUNG  on 10/19/20.
+//  Copyright © 2020 cosync. All rights reserved.
+//
+
+
+import Foundation
+import GoogleSignIn
+
+
+@available(iOS 15, *)
+public class CosyncGoogleAuth: ObservableObject {
+    
+    
+    @Published var isLoggedIn: Bool = false
+    @Published var errorMessage: String = ""
+    @Published var idToken: String = ""
+    
+    public var googleClientID: String 
+    public var givenName: String = ""
+    public var familyName: String = ""
+    public var email: String = ""
+    public var userId: String = ""
+    public var profilePicUrl: String = ""
+     
+    
+    
+    init(googleClientID:String){
+        self.googleClientID = googleClientID
+    }
+    
+    func getUserData(){
+        
+        if(GIDSignIn.sharedInstance.currentUser != nil){
+            
+            let user = GIDSignIn.sharedInstance.currentUser
+           
+            guard let user = user else { return }
+           
+            let givenName = user.profile?.givenName
+            self.familyName = user.profile?.familyName ?? ""
+            let profilePicUrl = user.profile!.imageURL(withDimension: 100)!.absoluteString
+            self.givenName = givenName ?? ""
+            self.email = user.profile?.email ?? ""
+            self.userId = user.userID ?? ""
+            self.idToken = user.idToken?.tokenString ?? ""
+            self.profilePicUrl = profilePicUrl
+            self.isLoggedIn = true
+            
+        }
+    }
+    
+    
+    func signIn(){
+        clear()
+        guard let presentingViewController = (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first?.rootViewController else {return}
+
+        let config = GIDConfiguration(clientID: self.googleClientID)
+        GIDSignIn.sharedInstance.configuration = config
+        GIDSignIn.sharedInstance.signIn(withPresenting: presentingViewController, completion: { user, error in
+          
+            if let err = error?.localizedDescription{
+                self.errorMessage = err
+            }
+            else if(GIDSignIn.sharedInstance.currentUser != nil){
+                self.getUserData()
+            }
+            else {
+                self.errorMessage = "Something went wrong"
+            }
+        })
+   }
+    
+    func signOut(){
+        GIDSignIn.sharedInstance.signOut()
+        clear()
+   }
+    
+    func clear(){
+        self.isLoggedIn = false
+        self.idToken = ""
+        self.errorMessage = ""
+    }
+}
